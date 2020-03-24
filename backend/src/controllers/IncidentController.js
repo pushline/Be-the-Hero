22c80119a43e68731bdcb0892ff1e42524d7620b
pushline <pushl1n3@gmail.com>
2020-03-24 19:54:00 -0300

    Back-end da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -46,7 +46,7 @@ module.exports = {
         .select('ong_id')
         .first();
 
-        if(incident.ong_id != ong_id){
+        if(incident.ong_id !== ong_id){
             return response.status(401).json({error :'Operation not permitted.'});
         }
         await connection('incidents').where('id', id).delete();
